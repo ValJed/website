ccb9ca756056ee16fb559456ee340a342cec33a9
Val <valentin.jeudy@gmail.com>
2021-05-17 23:33:01 +0200

    adds eslint plugin import and vscode settings

--- src/lib/three.js
+++ src/lib/three.js
@@ -8,62 +8,80 @@ import {
   PlaneGeometry,
   DoubleSide,
   HemisphereLight,
-  AmbientLight
+  AmbientLight,
+  Camera
 } from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
 
 let scene, camera, loader, renderer, model, controls
 
+const createCube = () => {
+  const geometry = new BoxGeometry()
+  const material = new MeshBasicMaterial({ color: 0x00ff00 })
+  const cube = new Mesh(geometry, material)
+
+  cube.position.set(0, 0, 0)
+  scene.add(cube)
+}
+
 const animate = () => {
-  requestAnimationFrame(animate);
-  model.rotation.z += 0.01;
+  requestAnimationFrame(animate)
+  model.rotation.z += 0.01
   // model.rotation.y += 0.01;
-  renderer.render(scene, camera);
-};
+  renderer.render(scene, camera)
+}
 
 const addLight = () => {
   // const hemiLight = new HemisphereLight(0xffeeb1, 0x080820, 4)
-  const hemiLight = new AmbientLight(0x404040, 7)
-  scene.add(hemiLight)
+  const light = new AmbientLight(0x404040, 7)
+  scene.add(light)
 }
 
 const resize = (width, height) => {
   renderer.setSize(width, height)
-  camera.position.set(1, 1, 1)
-  controls.update();
-};
+  camera.position.set(0, 0, 5)
+  camera.lookAt(0, 0, 0)
+  // controls.update();
+}
 
 const init = (el, width, height) => {
-  scene = new Scene();
-  camera = new PerspectiveCamera(100, width / height, 0.1, 1000);
-  loader = new GLTFLoader();
-  renderer = new WebGLRenderer({ antialias: true, canvas: el, alpha: true });
-  controls = new OrbitControls(camera, renderer.domElement);
+  scene = new Scene()
+  camera = new PerspectiveCamera(100, width / height, 0.1, 1000)
+  loader = new GLTFLoader()
+  renderer = new WebGLRenderer({
+    antialias: true,
+    canvas: el
+    // alpha: true
+  })
+  controls = new PointerLockControls(camera, renderer.domElement)
+
 }
 
 export const createScene = (el, width, height) => {
   try {
     init(el, width, height)
-
+    createCube()
 
     loader.load('models/samuraiMask/scene.gltf', (gltf) => {
       model = gltf.scene.children[0]
-      // model.scale.set(0.1, 0.1, 0.1)
+      model.scale.set(0.5, 0.5, 0.5)
       model.position.set(1, 1, 1)
 
-      scene.add(gltf.scene);
-      resize(width, height);
-      addLight()
+      resize(width, height)
 
-      renderer.render(scene, camera);
+      scene.add(gltf.scene)
+      // addLight()
+
+      createCube()
+
+      renderer.render(scene, camera)
       // animate()
-    });
-
-
+    })
   } catch (err) {
     console.error('Error when creatin scene: ', err)
   }
 }
 
-window.addEventListener('resize', resize);
+window.addEventListener('resize', resize)
