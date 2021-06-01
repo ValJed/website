883992b90a77d6c371605211d2effa32a6fc5c01
Val <valentin.jeudy@gmail.com>
2021-06-01 23:27:19 +0200

    interests block with dynamic typing

--- src/components/Interests.svelte
+++ src/components/Interests.svelte
@@ -1,32 +1,51 @@
 <script>
   import { onMount } from 'svelte'
 
-  const listElems = []
-  // your script goes here
   export let texts
+
+  const listElems = []
+  const min = 30
+  const max = 200
 
   onMount(() => {
     write(listElems)
   })
 
-  function write(listElems) {
-    listElems.forEach((el, i) => {
+  async function write(listElems) {
+    for (const [index, el] of Object.entries(listElems)) {
+      const i = parseInt(index, 10)
       el.classList.add('active')
-      console.log('el ===> ', el)
-      console.log('i ===> ', i)
+
+      for (const char of texts[i]) {
+        await timeOut()
+        el.append(char)
+      }
+
+      if (i !== listElems.length - 1) {
+        el.classList.remove('active')
+      }
+    }
+  }
+
+  function timeOut() {
+    return new Promise((resolve, reject) => {
+      const random = Math.random() * (max - min + 1) + min
+
+      setTimeout(() => {
+        resolve()
+      }, random)
     })
   }
 </script>
 
-<ul class="interests-list text-xl">
+<ul class="pl-4 text-xl">
   {#each texts as text, i}
-    <li class="active" bind:this={listElems[i]} />
+    <li class="my-4"><span bind:this={listElems[i]} /></li>
   {/each}
 </ul>
 
-<!-- markup (zero or more items) goes here -->
-<style>
-  .active {
+<style lang="scss">
+  :global(.active) {
     @apply border-green;
 
     animation: 1s infinite step-end blink;
@@ -36,10 +55,6 @@
     0% {
       @apply border-r-2;
     }
-
-    /* 50% {
-      @apply border-r-0;
-    } */
 
     50% {
       @apply border-r-0;
