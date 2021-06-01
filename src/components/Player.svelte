1e853a17a9924e6ecd78032ed99537791045bd95
Val <valentin.jeudy@gmail.com>
2021-06-01 23:27:27 +0200

    crates yt player

---
+++ src/components/Player.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<div>
+  <div class="player-header">toto</div>
+  <!-- <iframe
+    width="300"
+    height="200"
+    color="#269b00"
+    src="https://www.youtube.com/embed/iurQiz3Iv4I?controls=0"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  /> -->
+
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/KTIl1MugsSY?autoplay=1"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  />
+</div>
+
+<style>
+  .player-header {
+    @apply h-5 w-32 bg-green;
+
+    width: 300px;
+  }
+  /* your styles go here */
+</style>
