df9f9d094709ed182e11e3809ee5c2d65683e4e9
Jed <valentin.jeudy@gmail.com>
2021-10-24 22:49:57 +0200

    moves all three code from class to function

--- components/lib/three.js
+++ components/lib/three.js
@@ -12,96 +12,99 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 // import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-export class Samourai {
-  constructor(canvas, width, height) {
-    this.init(canvas, width, height)
+export const generateModel = async (canvas, width, height) => {
+  let mouseX
+  let mouseY
 
-    this.camera.position.set(0, 0, 200)
-    this.camera.lookAt(0, 0, 0)
+  const { scene, renderer, camera } = init(canvas, width, height)
+  // const controls = new OrbitControls(camera, renderer.domElement)
 
-    this.setModelCenter(height)
-    this.addLight()
-    this.getMousePosition()
+  const { model, gltfScene } = await loadModel()
 
-    this.loadModel(() => {
-      this.animate()
-    })
-  }
+  const { centerX, centerY } = setModelCenterAndPosition(model, height)
 
-  init(canvas, width, height) {
-    this.scene = new Scene()
-    this.camera = new PerspectiveCamera(45, width / height, 0.1, 1000)
-    this.renderer = new WebGLRenderer({
-      antialias: true,
-      canvas,
-      alpha: true
-    })
-    this.cubes = []
-    // this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    this.renderer.setSize(width, height)
-  }
+  const pivot = new Object3D()
 
-  setModelCenter(height) {
-    const windowWith = window.innerWidth
-    const windowHeight = window.innerHeight
+  pivot.add(model)
+  scene.add(pivot)
+  scene.add(gltfScene)
 
-    const centerOffset = height / 2 + 32
+  pivot.position.set(0, 0, -900)
 
-    this.modelCenterX = ((windowWith - centerOffset) / windowWith) * 100
-    this.modelCenterY = ((centerOffset + 300) / windowHeight) * 100
-  }
+  // Mouse position listener
+  document.addEventListener('mousemove', ({ clientX, clientY }) => {
+    mouseX = (clientX / window.innerWidth) * 100
+    mouseY = (clientY / window.innerHeight) * 100
+  })
 
-  loadModel(cb) {
-    this.loader = new GLTFLoader()
+  animate(pivot, renderer, scene, camera, { centerX, centerY, mouseX, mouseY })
 
-    this.loader.load('/models/samuraiMask/scene.gltf', (gltf) => {
-      this.model = gltf.scene.children[0]
+  function animate() {
+    requestAnimationFrame(animate)
 
-      // Getting size of model to position it (only y used)
-      const box = new Box3().setFromObject(this.model)
-
-      const { y } = box.getSize(new Vector3())
-
-      this.model.position.set(0, -y, 0)
-      this.pivot = new Object3D()
-
-      this.pivot.add(this.model)
-      this.scene.add(this.pivot)
-      this.scene.add(gltf.scene)
-
-      this.pivot.position.set(0, 0, -900)
-
-      cb()
-    })
-  }
-
-  getMousePosition() {
-    document.addEventListener('mousemove', ({ clientX, clientY }) => {
-      this.mouseX = (clientX / window.innerWidth) * 100
-      this.mouseY = (clientY / window.innerHeight) * 100
-    })
-  }
-
-  followMouse() {
-    if (this.mouseX && this.mouseY) {
-      this.pivot.rotation.y = ((this.mouseX - this.modelCenterX) / 100) * 1.2
-      this.pivot.rotation.x = ((this.mouseY - this.modelCenterY) / 100) * 1.2
-    }
-  }
-
-  addLight() {
-    this.scene.add(new AmbientLight(0x9b9898, 6))
-  }
-
-  animate() {
-    requestAnimationFrame(this.animate.bind(this))
-
-    if (this.pivot.position.z < -100) {
-      this.pivot.position.set(0, 0, this.pivot.position.z + 30)
-    } else {
-      this.followMouse()
+    if (pivot.position.z < -100) {
+      pivot.position.set(0, 0, pivot.position.z + 20)
+    } else if (mouseX && mouseY) {
+      pivot.rotation.y = ((mouseX - centerX) / 100) * 1.2
+      pivot.rotation.x = ((mouseY - centerY) / 100) * 1.2
     }
 
-    this.renderer.render(this.scene, this.camera)
+    renderer.render(scene, camera)
   }
+}
+
+function init(canvas, width, height) {
+  const camera = new PerspectiveCamera(45, width / height, 0.1, 1000)
+  const scene = new Scene()
+  const renderer = new WebGLRenderer({
+    antialias: true,
+    canvas,
+    alpha: true
+  })
+
+  // Camera setting
+  camera.position.set(0, 0, 200)
+  camera.lookAt(0, 0, 0)
+
+  // Light
+  scene.add(new AmbientLight(0x9b9898, 6))
+
+  renderer.setSize(width, height)
+
+  return {
+    scene,
+    camera,
+    renderer,
+  }
+}
+
+function setModelCenterAndPosition(model, height) {
+  // Mode position
+  const box = new Box3().setFromObject(model)
+  const { y } = box.getSize(new Vector3())
+  model.position.set(0, -y, 0)
+
+  // Model center
+  const windowWith = window.innerWidth
+  const windowHeight = window.innerHeight
+  const centerOffset = height / 2 + 32
+
+  return {
+    centerX: ((windowWith - centerOffset) / windowWith) * 100,
+    centerY: ((centerOffset + 300) / windowHeight) * 100
+  }
+}
+
+function loadModel() {
+  const loader = new GLTFLoader()
+
+  return new Promise((resolve, reject) => {
+    loader.load('/models/samuraiMask/scene.gltf', (gltf) => {
+      const [model] = gltf.scene.children
+
+      if (model) {
+        resolve({ model, gltfScene: gltf.scene })
+      }
+    })
+  })
 }
