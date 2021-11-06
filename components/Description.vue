6675a9d30503dc1aad2a439c1f516a8b02b2b363
Jed <vjeudy@protonmail.com>
2021-11-06 20:14:12 +0100

    fixes weapons animation and move it to lib

--- components/Description.vue
+++ components/Description.vue
@@ -27,12 +27,11 @@
 <script>
 import { defineComponent, onMounted, ref } from '@nuxtjs/composition-api'
 import { DateTime } from 'luxon'
+import weaponsAnimation from '@/lib/weaponsAnimation'
 
 // import Node from '@/components/svg/Node.vue'
 // import Vue from '@/components/svg/Vue.vue'
 // import Mongodb from '@/components/svg/Mongodb.vue'
-
-const japaneseChars = '悪魔モンスター絶望悲しみ愛する平和エントピ'
 
 export default defineComponent({
   props: {
@@ -46,14 +45,12 @@ export default defineComponent({
 
     const seconds = ref(0)
     const weapon = ref(weapons[0])
-    const iter = ref(0)
 
     onMounted(() => {
       computeDiffDate(start, seconds)
 
       setInterval(async () => {
-        await computeWeapons(weapons, weapon)
-        console.log('iter.value ===> ', iter.value)
+        await weaponsAnimation(weapons, weapon)
       }, 3000)
     })
 
@@ -69,73 +66,6 @@ export default defineComponent({
       setTimeout(() => {
         computeDiffDate(start, sec)
       }, 1000)
-    }
-
-    async function computeWeapons(weapons, weapon) {
-      const currentIndex = weapons.indexOf(weapon.value)
-      const nextIndex =
-        currentIndex + 1 !== weapons.length ? currentIndex + 1 : 0
-      const nextWeapon = weapons[nextIndex]
-      const longerWordLength =
-        nextWeapon.length >= weapon.value.length
-          ? nextWeapon.length
-          : weapon.value.length
-      // const longerWord =
-      //   nextWeapon.length >= weapon.value.length ? nextWeapon : weapon.value
-
-      await weaponToJapanese(weapon)
-
-      await japaneseToWeapon(weapon, nextWeapon, longerWordLength)
-
-      // await computeWeapons(weapons, weapon)
-
-      async function weaponToJapanese(weapon) {
-        const randomIndexes = getRandomIndexes(weapon.value.length)
-        for (const index of randomIndexes) {
-          const randomJapaneseIndex = Math.floor(
-            Math.random() * (japaneseChars.length - 0 + 1) + 0
-          )
-          weapon.value = replaceChar(
-            weapon.value,
-            index,
-            japaneseChars[randomJapaneseIndex]
-          )
-
-          await timeOut(50)
-        }
-      }
-
-      async function japaneseToWeapon(weapon, nextWeapon, longerWordLength) {
-        const randomIndexes = getRandomIndexes(longerWordLength)
-
-        for (const index of randomIndexes) {
-          weapon.value = replaceChar(weapon.value, index, nextWeapon[index])
-
-          await timeOut(150)
-        }
-      }
-
-      function getRandomIndexes(length) {
-        return [...Array(length).keys()].sort(() =>
-          Math.random() > 0.5 ? 1 : -1
-        )
-      }
-
-      function replaceChar(word, index, char) {
-        const splitted = word.split('')
-
-        splitted[index] = char || ''
-
-        return splitted.join('')
-      }
-
-      function timeOut(time) {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            resolve()
-          }, time)
-        })
-      }
     }
   }
 })
