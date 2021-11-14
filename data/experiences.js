20be343cb7eb7f8705072770ff0b7a6fefe3b15b
Jed <vjeudy@protonmail.com>
2021-11-14 22:24:11 +0100

    adds experiences content file

---
+++ data/experiences.js
@@ -0,0 +1,52 @@
+export default {
+  apostrophe: {
+    name: 'Apostrophe',
+    logoName: "Apostrophe",
+    date: 'November 2020 - Today',
+    img: 'apostrophe.svg',
+    stack: "Nodejs / Vue / Mongodb",
+    content: [
+      'Developing / improving ApostropheCMS core and modules. Setting up and working on ApostropheCMS projects for customers.'
+    ]
+  },
+  easylife: {
+    name: 'EasyLife',
+    date: 'November 2019 - July 2020',
+    img: 'easylife.svg',
+    stack: 'Nodejs / React / Mssql / Sequelize / Elasticsearch',
+    content: [
+      'Realization of a business tool for a corporate concierge.',
+      'Managing tickets, products, collaborators through a single application.',
+      'Development of a customer application allowing employees to order products / services online.',
+      'Creation of a chatbot that allow users to get information (contacts, hours, etc.) as well as to make requests.',
+      'Smart search system with Elasticsearch.'
+    ]
+  },
+  iboycott: {
+    name: 'Iboycott',
+    date: 'November 2020 - Today',
+    img: 'iboycott.png',
+    stack: "Nodejs / React / SQL",
+    content: [
+      'Developing / improving ApostropheCMS core and modules. Setting up and working on ApostropheCMS projects for customers.'
+    ]
+  },
+  michelin: {
+    name: 'Michelin',
+    date: 'May 2017 - November 2019',
+    img: 'michelin.png',
+    stack: 'Nodejs / Vue / MongoDB',
+    content: [
+      'multilingual sites conception for the different brands of the group.',
+      'Delivering a large amount of websites through our ApostropheCMS factory.',
+      'Creation of cross sites applications (Dealer locator / Rating & reviews / Tyre selector)'
+    ]
+  },
+  dedi: {
+    name: 'Dedi Agency',
+    date: 'April - September 2016',
+    img: 'dedi.png',
+    stack: 'HTML / Sass',
+    content: ['Pixel perfect and responsive integrations for online shops.']
+  }
+}
