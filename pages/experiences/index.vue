885795a64e2ad2fa9e7e939753424b4d755c0242
Jed <vjeudy@protonmail.com>
2021-11-14 22:25:26 +0100

    moves experiences component to its folder

---
+++ pages/experiences/index.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="experiences">
+    <h1>Experiences</h1>
+    <ul>
+      <li>
+        <NuxtLink to="experiences/apostrophe">
+          <div>
+            <img src="/images/apostrophe.svg" alt="Apostrophe" />
+            <span>Apostrophe</span>
+          </div>
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="experiences/iboycott">
+          <img src="/images/iboycott.png" alt="I-boycott" />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="experiences/easylife">
+          <img src="/images/easylife.svg" alt="Easylife" />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="experiences/michelin">
+          <img src="/images/michelin.png" alt="Michelin" />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="experiences/dedi">
+          <img src="/images/dedi.png" alt="Dedi Agency" />
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { defineComponent, onMounted, ref } from '@nuxtjs/composition-api'
+import { matrixTransition } from '@/lib/transitions'
+
+export default defineComponent({
+  transition(to, from) {
+    return to.name === 'experiences-experience' && from.name === 'experiences'
+      ? { name: 'xp', duration: 5000 }
+      : matrixTransition
+  },
+  setup() {
+    const showXp = ref(false)
+    const active = ref(null)
+
+    onMounted(() => {
+      setTimeout(() => {
+        showXp.value = true
+      }, 250)
+    })
+
+    return {
+      showXp,
+      active
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  padding: 3rem 0;
+}
+
+.experiences {
+  ul {
+    position: relative;
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(2, auto);
+    height: calc(100% - 132px);
+  }
+
+  li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: all 0.3s linear;
+    border-radius: 10px;
+
+    &:hover {
+      box-shadow: 10px -10px 50px $green;
+    }
+
+    img {
+      display: block;
+      min-height: 2.5rem;
+      max-height: 5rem;
+      max-width: 13rem;
+      height: auto;
+    }
+
+    div {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      span {
+        font-size: 1.5rem;
+        font-family: Inter, 'sans-serif';
+        margin-left: 0.5rem;
+      }
+    }
+  }
+}
+
+h2 {
+  @include text-2xl;
+  font-family: $ftitle;
+  padding: 1.5rem 0;
+}
+
+// Animations
+
+// @keyframes showListItem {
+// }
+
+// @keyframes growbar {
+//   0% {
+//     width: 0;
+//   }
+
+//   100% {
+//     width: 80%;
+//   }
+// }
+
+// .page-enter-active {
+//   h3 {
+//     > span {
+//       &:after {
+//         animation: 0.5s linear 0.1s backwards running growbar;
+//       }
+//     }
+//   }
+// }
+</style>
