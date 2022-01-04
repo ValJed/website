275f2edddc0d786915fd45ea1e2886c3bbd5b03e
Jed <vjeudy@protonmail.com>
2022-01-04 23:28:32 +0100

    poc loading model through web worker

--- model.worker.js
+++ model.worker.js
@@ -1,14 +1,15 @@
-self.addEventListener('message', async () => {
-  const { GLTFLoader } = await import(
-    'three/examples/jsm/loaders/GLTFLoader'
-  )
-  const loader = new GLTFLoader()
+// import makeIterableTransferable from '@/lib/makeIterableTransferable'
 
-  loader.load('/models/samuraiMask/scene.gltf', (gltf) => {
-    const [model] = gltf.scene.children
+self.addEventListener('message', async (e) => {
 
-    if (model) {
-      postMessage({ model, gltfScene: gltf.scene })
-    }
-  })
+  //   const { GLTFLoader } = await import(
+  //     'three/examples/jsm/loaders/GLTFLoader'
+  //   )
+  //   const loader = new GLTFLoader()
+
+  //   loader.load('/models/samuraiMask/scene.gltf', (gltf) => {
+  //     const scene = makeIterableTransferable(gltf.scene)
+
+  //     // postMessage(scene)
+  //   })
 })
