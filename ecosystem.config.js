3234c465ef0525db1ad54a26d2565587420610da
Jed <vjeudy@protonmail.com>
2022-12-13 21:51:33 +0100

    updates pM2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,11 +11,11 @@ module.exports = {
       user: 'ubuntu',
       host: '51.210.243.88',
       ref: 'origin/master',
-      repo: 'git@github.com:ValJed/website.git',
+      repo: 'https://github.com/ValJed/website.git',
       path: '/var/www/website',
       'pre-deploy-local': '',
       'post-deploy':
-        'npm install && pm2 reload ecosystem.config.js --env production',
+        'npm install && npm run generate && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
