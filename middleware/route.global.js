9acb2c2e1cca4064d75719620833138d3b34e827
Jed <vjeudy@protonmail.com>
2023-02-21 00:33:42 +0100

    no setTimeout to scroll top on mobile

--- middleware/route.global.js
+++ middleware/route.global.js
@@ -1,7 +1,14 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  if (to.path !== from.path && process.client) {
-    setTimeout(() => {
-      window.scrollTo(0, 0)
-    }, 250)
+  if (to.path === from.path || !process.client) {
+    return
   }
+
+  if (window.innerWidth < 900) {
+    window.scrollTo(0, 0)
+    return
+  }
+
+  setTimeout(() => {
+    window.scrollTo(0, 0)
+  }, 250)
 })
