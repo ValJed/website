213c51b1b27593972a7c77da20e0b4442bc7ced7
Jed <vjeudy@protonmail.com>
2023-02-22 00:15:43 +0100

    reworks xp page arrow style

--- pages/experiences/[experience].vue
+++ pages/experiences/[experience].vue
@@ -1,31 +1,35 @@
 <template>
-  <div class="work">
-    <div class="logo">
-      <img :src="`/images/${experience.img}`" :alt="experience.name" />
-      <span v-if="experience.logoName">{{ experience.logoName }}</span>
+  <div>
+    <div class="work">
+      <div class="logo">
+        <img :src="`/images/${experience.img}`" :alt="experience.name" />
+        <span v-if="experience.logoName">{{ experience.logoName }}</span>
+      </div>
+      <div class="description">
+        <p>{{ experience.date }}</p>
+        <p class="stack" v-html="stack"></p>
+        <p v-for="(paragraph, i) in experience.content" :key="i">
+          {{ paragraph }}
+        </p>
+        <p v-if="experience.link">
+          See
+          <a class="link" target="__blank" :href="experience.link">{{
+            experience.link
+          }}</a>
+        </p>
+      </div>
     </div>
-    <div class="description">
-      <p>{{ experience.date }}</p>
-      <p class="stack" v-html="stack"></p>
-      <p v-for="(paragraph, i) in experience.content" :key="i">
-        {{ paragraph }}
-      </p>
-      <p v-if="experience.link">
-        See
-        <a class="link" target="__blank" :href="experience.link">{{
-          experience.link
-        }}</a>
-      </p>
+    <div class="back-link__container">
+      <NuxtLink class="back-link" to="/experiences">
+        <Spear />
+      </NuxtLink>
     </div>
-    <NuxtLink class="arrow" to="/experiences">
-      <Arrow />
-    </NuxtLink>
   </div>
 </template>
 
 <script setup>
 import experiences from '@/data/experiences'
-import Arrow from '@/components/svg/Arrow.vue'
+import Spear from '@/components/svg/Spear.vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
@@ -65,6 +69,7 @@ img {
 
   @include tablet-landscape {
     margin-top: 3rem;
+    padding-bottom: 6rem;
   }
 }
 
@@ -80,15 +85,31 @@ img {
   color: $green;
 }
 
-.arrow {
+.back-link {
   display: inline-block;
-  width: 4rem;
-  height: 4rem;
-  margin: 3rem 0 3rem 1.24rem;
+  transform: rotate(-90deg);
+  width: 8rem;
+  height: 8rem;
+}
+
+.back-link__container {
+  position: static;
+  display: flex;
+  align-items: center;
 
   @include tablet-landscape {
-    position: absolute;
-    bottom: 5rem;
+    position: fixed;
+    height: 5rem;
+    background-color: $black;
+    bottom: 0;
+    left: 10%;
+    width: 80%;
+    overflow: hidden;
+  }
+
+  @include desktop {
+    max-width: 1400px;
+    left: calc((100vw - 1400px) / 2);
   }
 }
 
