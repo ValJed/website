1453d02b87815c0d104739dc51a550b1d41059a8
Jed <vjeudy@protonmail.com>
2023-02-22 19:53:26 +0100

    improves head description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,23 +12,23 @@ export default defineNuxtConfig({
   // Global page headers: https://go.nuxtjs.dev/config-head
   app: {
     head: {
-      title: 'Valentin Jeudy Developer',
+      title: 'Valentin Jeudy - Full Stack Web Developer',
       htmlAttrs: {
         lang: 'en'
       },
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
-        { name: 'format-detection', content: 'telephone=no' }
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            "Full stack web developer based in France. I love to collaborate with people on great and useful tools, let's try to make the world even a little bit better."
+        }
       ],
       link: [{ rel: 'icon', href: '/favicon.svg' }]
     }
   },
-
-  // routeRules: {
-  //   '/_nuxt/**': { headers: { 'Access-Control-Allow-Origin': '*' } }
-  // },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   // css: ['@/assets/scss/main'],
@@ -41,9 +41,6 @@ export default defineNuxtConfig({
       }
     }
   },
-  // styleResources: {
-  //   scss: ['@/assets/scss/mixins/*.scss']
-  // },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
