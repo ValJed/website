1de37d430ae3c3d9ad09aa39b4e48d10a2a4140c
Jed <vjeudy@protonmail.com>
2025-04-20 22:38:39 +0200

    removes server code

--- server/api/article.ts
+++
@@ -1,5 +0,0 @@
-import { getQuery } from 'h3'
-export default defineEventHandler(async (event) => {
-  const { article } = getQuery(event)
-  return $api(`/articles/by-slug/${article}`)
-})
--- server/api/experience.ts
+++
@@ -1,5 +0,0 @@
-import { getQuery } from 'h3'
-export default defineEventHandler(async (event) => {
-  const { experience } = getQuery(event)
-  return $api(`/experiences/by-slug/${experience}`)
-})
